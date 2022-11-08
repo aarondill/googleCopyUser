c1c4b5aa972fd530424a4719a4413a2d13d68bc1
aarondill <aaronsacks2006@gmail.com>
2022-11-07 22:42:33 -0600

    merged files into copy_page.js
    removed with_account and without_account and merged into copy_page.js
    which handles logic for both

--- js/copy_page.js
+++ js/copy_page.js
@@ -1,6 +1,7 @@
 /*global chrome*/
 (async function () {
 	const constructDiv = (url, array) => {
+		if (url[0].match(/\/u\/\d$/)) url[0] = url[0].slice(0, -4);
 		let r = `<div style="margin-top: -5px;margin-bottom: 5px;font-size: 16px;font-weight: bold;">Click to copy with: </div>`;
 		for (let i = 0; i < array.length; i++) {
 			r += `<div style="padding:5px 0"><a href="${url[0]}/u/${i}/d/${url[1]}">${array[i]}</a></div>`;
@@ -9,13 +10,17 @@
 	};
 	const fileUrlAccountsJS = chrome.runtime.getURL(".accounts.js");
 	const accounts = (await import(fileUrlAccountsJS)).default;
+	const urlUserRegex = /https:\/\/docs\.google\.com\/[a-z]+\/u\/(?<u>\d).+/;
+	const matches = location.href.match(urlUserRegex);
+	const currentUser = accounts[matches?.groups.u ?? 0];
+
 	const linksDiv = document.createElement("div");
 	linksDiv.innerHTML = constructDiv(location.href.split("/d/"), accounts);
 	linksDiv.style =
 		"position:absolute; top:0; right:0; padding:10px; border:solid black 1px";
 	document.body.prepend(linksDiv);
 	const currentAccountDiv = document.createElement("div");
-	currentAccountDiv.innerHTML = `Signed in: ${accounts[0]}`;
+	currentAccountDiv.innerHTML = `Signed in: ${currentUser}`;
 	currentAccountDiv.style = `position: absolute; top: 0px;
 	font-weight: bold; font-size: 12pt;
 	outline: solid 2px; border-radius: 20vw;
