ac5ecd16759988fe93b133333dd722fa18d5053e
aarondill <aaronsacks2006@gmail.com>
2022-11-07 22:59:07 -0600

    Added injected stylesheet to page

--- js/copy_page.js
+++ js/copy_page.js
@@ -2,9 +2,9 @@
 (async function () {
 	const constructDiv = (url, array) => {
 		if (url[0].match(/\/u\/\d$/)) url[0] = url[0].slice(0, -4);
-		let r = `<div style="margin-top: -5px;margin-bottom: 5px;font-size: 16px;font-weight: bold;">Click to copy with: </div>`;
+		let r = `<div id="injectedheading">Click to copy with: </div>`;
 		for (let i = 0; i < array.length; i++) {
-			r += `<div style="padding:5px 0"><a href="${url[0]}/u/${i}/d/${url[1]}">${array[i]}</a></div>`;
+			r += `<div><a class="injected-a" href="${url[0]}/u/${i}/d/${url[1]}">${array[i]}</a></div>`;
 		}
 		return r;
 	};
@@ -14,16 +14,37 @@
 	const matches = location.href.match(urlUserRegex);
 	const currentUser = accounts[matches?.groups.u ?? 0];
 
+	const styleSheet = document.createElement("style");
+	styleSheet.innerText = `
+#injectedheading{
+	margin-top: -5px; margin-bottom: 5px;
+	font-size: 16px; font-weight: bold;
+}
+div:has(.injected-a){
+	padding: 5px 0;
+}
+.injected-a{
+color: #112ABB; font-family: Arial,sans-serif;
+font-size: 13px; cursor: pointer; text-decoration: underline;
+}
+#linksDiv {
+	position: absolute; top: 0; right: 0;
+	padding: 10px; border: solid black 1px; z-index: 10
+}
+#currentAccount{
+	position: absolute; top: 0px;
+	font-weight: bold; font-size: 12pt;
+	outline: solid 2px; border-radius: 20vw;
+	padding: 10px; background: #4c8ffb4f; z-index: 10
+}
+`;
+	document.head.prepend(styleSheet);
 	const linksDiv = document.createElement("div");
 	linksDiv.innerHTML = constructDiv(location.href.split("/d/"), accounts);
-	linksDiv.style =
-		"position:absolute; top:0; right:0; padding:10px; border:solid black 1px";
+	linksDiv.id = "linksDiv";
 	document.body.prepend(linksDiv);
 	const currentAccountDiv = document.createElement("div");
 	currentAccountDiv.innerHTML = `Signed in: ${currentUser}`;
-	currentAccountDiv.style = `position: absolute; top: 0px;
-	font-weight: bold; font-size: 12pt;
-	outline: solid 2px; border-radius: 20vw;
-	padding: 10px; background: #4c8ffb4f;`;
+	currentAccountDiv.id = "currentAccount";
 	document.body.prepend(currentAccountDiv);
 })();
