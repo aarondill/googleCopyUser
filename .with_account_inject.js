0192044268b36b537a0ebae45d7d817888ce7599
aarondill <aaronsacks2006@gmail.com>
2022-11-07 21:35:07 -0600

    add injection code for with_account.js

--- .with_account_inject.js
+++ .with_account_inject.js
@@ -1,8 +1,5 @@
-/*global chrome*/
-//todo, let user know current account
 (async function () {
-	const fileUrlAccountsJS = chrome.runtime.getURL(".accounts.js");
-	let accounts = await import(fileUrlAccountsJS);
+	accounts = await import(this.getAttribute("data-url"));
 	accounts = accounts.default;
 	const matches = location.href.match(
 		/https:\/\/docs\.google\.com\/[a-z]+\/u\/(?<u>\d).+/
@@ -10,5 +7,5 @@
 	const text = document.createElement("div");
 	text.style = "position:absolute; top:0; right:0; padding:10px";
 	text.innerText = `Signed in to ${accounts[matches.groups.u]}`;
-	document.body.prepend(text);
-})();
+	this.parentElement.replaceChild(text, this);
+}.apply(document.currentScript));
