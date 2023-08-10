8e7825aba8398cde2cd6aa9eef0e711eb8a5253a
aarondill <aaronsacks2006@gmail.com>
2023-08-10 09:00:06 -0500

    update packExtension to search other executables

--- packExtension.sh
+++ packExtension.sh
@@ -2,17 +2,40 @@
 # Warning, only works if called by a file path!!
 rootfolder=$(realpath -- "$(dirname -- "$0")")
 rootfoldername=$(basename -- "$rootfolder")
-if ! [[ -d $rootfolder ]]; then
-    printf 'Something went wrong when finding your file. This can only be called with a file path'
+err() { printf '%s\n' "$@" >&2; }
+abort() {
+  err "$1"
+  exit "${2:-1}"
+}
+has() { command -v -- "$1" >/dev/null 2>&1; }
+if ! [ -d "$rootfolder" ]; then
+  abort 'Something went wrong when finding your file. This can only be called with a file path.' 1
+fi
+
+cmds=(
+  "${GOOGLE_CHROME:-}" # allow user to override
+  google-chrome google-chrome-stable chromium
+  chromium-browser vivaldi brave
+)
+chrome=
+for f in "${cmds[@]}"; do
+  if has "$f"; then
+    chrome="$f"
+    break
+  fi
+done
+
+if [ -z "$chrome" ]; then
+  abort "Couldn't find chrome executable to package extension. Try setting the GOOGLE_CHROME environment variable or check if you have it installed." 2
 fi
 
 keyfile=$rootfolder/$rootfoldername.pem
 if [[ -f "$keyfile" ]]; then
   # Key file exists at root file
-  google-chrome --pack-extension="$rootfolder/src" --pack-extension-key="$keyfile" || exit
+  "$chrome" --pack-extension="$rootfolder/src" --pack-extension-key="$keyfile" || exit
 else
   # No key file is present
-  google-chrome --pack-extension="$rootfolder/src" || exit
+  "$chrome" --pack-extension="$rootfolder/src" || exit
 fi
 
 distfolder=$rootfolder/dist
