f18f5daf04de2ba021b040749ae70d671ad1ce5a
Aaron Dill <aaronsacks2006@gmail.com>
2025-01-28 00:38:52 -0600

    fix: don't use dynamic imports

--- src/js/copy_page.js
+++ src/js/copy_page.js
@@ -1,23 +1,21 @@
 /*global chrome*/
 (async function () {
-	function constructDiv(url, array) {
-		if (url[0].match(/\/u\/\d$/)) url[0] = url[0].slice(0, -4);
-		let r = `<div id="injected-heading">Click to copy with: </div>`;
-		for (let i = 0; i < array.length; i++) {
-			r += `<div><a class="injected-a" href="${url[0]}/u/${i}/d/${url[1]}">${array[i]}</a></div>`;
-		}
-		return r;
-	}
-	const fileUrlAccountsJS = chrome.runtime.getURL("accounts.js");
-	const accounts = await import(fileUrlAccountsJS).then(i => i.default);
-	const urlUserRegex = /https:\/\/[a-z]+\.google\.com\/[a-z]+\/u\/(?<u>\d).+/i;
-	const matches = location.href.match(urlUserRegex);
-	const currentUser = accounts[matches?.groups.u ?? 0];
-	const styleSheet = document.createElement("style");
-	const linksDiv = document.createElement("div");
-	const currentAccountDiv = document.createElement("div");
+  function constructDiv(url, array) {
+    if (url[0].match(/\/u\/\d$/)) url[0] = url[0].slice(0, -4);
+    let r = `<div id="injected-heading">Click to copy with: </div>`;
+    for (let i = 0; i < array.length; i++) {
+      r += `<div><a class="injected-a" href="${url[0]}/u/${i}/d/${url[1]}">${array[i]}</a></div>`;
+    }
+    return r;
+  }
+  const urlUserRegex = /https:\/\/[a-z]+\.google\.com\/[a-z]+\/u\/(?<u>\d).+/i;
+  const matches = location.href.match(urlUserRegex);
+  const currentUser = accounts[matches?.groups.u ?? 0];
+  const styleSheet = document.createElement("style");
+  const linksDiv = document.createElement("div");
+  const currentAccountDiv = document.createElement("div");
 
-	styleSheet.innerText = `
+  styleSheet.innerText = `
 #injected-heading{
 	margin-top: -5px; margin-bottom: 5px;
 	font-size: 16px; font-weight: bold;
@@ -40,11 +38,11 @@ font-size: 13px; cursor: pointer; text-decoration: underline;
 	padding: 10px; background: #4c8ffb4f; z-index: 10
 }
 `;
-	document.head.prepend(styleSheet);
-	linksDiv.innerHTML = constructDiv(location.href.split("/d/"), accounts);
-	linksDiv.id = "linksDiv";
-	document.body.prepend(linksDiv);
-	currentAccountDiv.innerHTML = `Signed in: ${currentUser}`;
-	currentAccountDiv.id = "currentAccount";
-	document.body.prepend(currentAccountDiv);
+  document.head.prepend(styleSheet);
+  linksDiv.innerHTML = constructDiv(location.href.split("/d/"), accounts);
+  linksDiv.id = "linksDiv";
+  document.body.prepend(linksDiv);
+  currentAccountDiv.innerHTML = `Signed in: ${currentUser}`;
+  currentAccountDiv.id = "currentAccount";
+  document.body.prepend(currentAccountDiv);
 })();
